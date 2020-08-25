def5ec68084dc354284f55355e51ec52db8dd0e8
unknown <bogdanlikuliks@hotmail.com>
2020-08-25 14:29:45 +0200

    Bouncing Balls - more bouncing balls - ver 1.02

--- Bouncing Balls/app.js
+++ Bouncing Balls/app.js
@@ -3,7 +3,6 @@ canvas.width = 905;
 canvas.height = 550;
 let ctx = canvas.getContext('2d');
 
-
 canvas.style.border = '2px solid wheat';
 canvas.style.backgroundColor = 'rgb(27, 27, 27)';
 
@@ -33,18 +32,34 @@ class Ball {
     }
     draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 30, 0, Math.PI * 2, false);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.strokeStyle = 'red';
         ctx.stroke();
     }
 }
 
-let ball = new Ball(200, 200, 2, 2, 30);
+// let ball = new Ball(200, 200, 2, 2, 30);
 
+
+let balls = [];
+
+for (let i = 0; i < 10; i++) {
+    let radius = Math.floor(Math.random() * 26 + 5); // form 5 to 30 int
+    console.log(radius)
+    let x = Math.random() * (canvas.width - radius * 2) + radius;
+    let y = Math.random() * (canvas.height - radius * 2) + radius;
+    let x_velocity = (Math.random() - 0.5) * 2;
+    let y_velocity = (Math.random() - 0.5) * 2;
+    balls.push(new Ball(x, y, x_velocity, y_velocity, radius))
+
+}
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     requestAnimationFrame(animate);
-    ball.update();
+    // ball.update();
+    balls.forEach(ball => {
+        ball.update();
+    });
 }
 
 animate();
