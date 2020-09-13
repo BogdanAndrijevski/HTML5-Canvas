586f6d92f58ef513cdfc4c908a093d468a7c35dd
unknown <bogdanlikuliks@hotmail.com>
2020-09-13 17:22:22 +0200

    Balls Bounce Off Each Other - making the balls to move and adding utility functions - ver 1.02

--- Balls Bounce Off Each Other/app.js
+++ Balls Bounce Off Each Other/app.js
@@ -27,15 +27,52 @@ function getDistance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2))
 }
 
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function distance(x1, y1, x2, y2) {
+    const xDist = x2 - x1
+    const yDist = y2 - y1
+
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+}
+
 class Ball {
     constructor(x, y, radius, color) {
         this.x = x;
         this.y = y;
+        this.velocity = {
+            x: Math.random() - 0.5,
+            y: Math.random() - 0.5
+        }
         this.radius = radius;
         this.color = color;
     }
-    update() {
+    update(particles) {
+        for (let i = 0; i < particles.length; i++) {
+            if (particles[i] === this) continue;
+            if (getDistance(this.x, this.y, particles[i].x, particles[i].y) - (this.radius * 2) < 0) {
+                console.log('has colided')
+            }
+        }
 
+        if (this.x + this.radius > canvas.width ||
+            this.x - this.radius < 0) {
+            this.velocity.x = - this.velocity.x;
+        }
+
+        if (this.y + this.radius > canvas.height ||
+            this.y - this.radius < 0) {
+            this.velocity.y = - this.velocity.y;
+        }
+
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
         this.draw();
     }
     draw() {
@@ -52,15 +89,17 @@ function init() {
     balls = [];
     for (let i = 0; i < 4; i++) {
         const color = 'yellow';
-        const radius = 100;
-        let x = Math.random() * innerWidth
-        let y = Math.random() * innerHeight
+        const radius = 50;
+        let x = randomIntFromRange(radius, canvas.width - radius)
+        // let x = Math.random() * innerWidth
+        let y = randomIntFromRange(radius, canvas.height - radius)
+        // let y = Math.random() * innerHeight
 
         if (i != 0) {
             for (let j = 0; j < balls.length; j++) {
                 if (getDistance(x, y, balls[j].x, balls[j].y) - (radius * 2) < 0) {
-                    x = Math.random() * innerWidth
-                    y = Math.random() * innerHeight
+                    x = randomIntFromRange(radius, canvas.width - radius)
+                    y = randomIntFromRange(radius, canvas.height - radius)
                     j = -1;
                 }
             }
@@ -74,7 +113,7 @@ function animate() {
     requestAnimationFrame(animate);
 
     balls.forEach(e => {
-        e.update();
+        e.update(balls);
     });
 }
 
