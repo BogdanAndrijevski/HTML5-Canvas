594410f5de03782d889fa6202bba29b09a9af1f3
unknown <bogdanlikuliks@hotmail.com>
2020-09-27 23:54:23 +0200

    Coding Gravity - more balls - ver 1.03

--- Coding Gravity/app.js
+++ Coding Gravity/app.js
@@ -15,6 +15,10 @@ addEventListener('resize', () => {
     init()
 })
 
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 class Ball {
     constructor(x, y, y_velocity, radius, color) {
         this.x = x;
@@ -24,8 +28,8 @@ class Ball {
         this.y_velocity = y_velocity
     }
     update() {
-        if (this.y + this.radius > canvas.height) {
-            this.y_velocity = -this.y_velocity * friction; 
+        if (this.y + this.radius + this.y_velocity > canvas.height) {
+            this.y_velocity = -this.y_velocity * friction;
         } else {
             this.y_velocity += gravity;
         }
@@ -41,15 +45,24 @@ class Ball {
 }
 
 let ball;
-
+let balls;
 
 function init() {
+    balls = [];
+    for (let i = 0; i < 10; i++) {
+        let radius = randomIntFromRange(15, 20)
+        let x = randomIntFromRange(radius, canvas.width - radius)
+        let y = randomIntFromRange(radius, canvas.height - radius)
+        balls.push(new Ball(x, y, 1, radius, 'red'))
+    }
     ball = new Ball(canvas.width / 2, canvas.height / 2, 1, 20, 'red');
-
 }
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ball.update();
+    balls.forEach(ball => {
+        ball.update()
+    });
     requestAnimationFrame(animate);
 
 }
