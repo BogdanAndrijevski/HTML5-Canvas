6fcf9e11f97b0d01a92ba148d87eeaa965845f68
unknown <bogdanlikuliks@hotmail.com>
2020-10-01 23:57:22 +0200

    Star Shower - first commit - ver 1.00

---
+++ Star Shower/app.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+canvas.style.border = '2px solid wheat';
+canvas.style.backgroundColor = 'rgb(11, 11, 11)';
+canvas.width = innerWidth
+canvas.height = innerHeight
