09e4bf69b663c0294b4bd6e76f11a0648e441529
unknown <bogdanlikuliks@hotmail.com>
2020-10-12 04:34:51 +0200

    Fireworks - adding color and more veloocity to the particles - ver 1.03

--- Fireworks/app.js
+++ Fireworks/app.js
@@ -21,12 +21,15 @@ addEventListener('click', e => {
   mouse.y = e.clientY
   const particleCount = 10
   const angleIncrement = Math.PI * 2 / particleCount
-
+  const power = 2
   for (let i = 0; i < particleCount; i++) {
-    particles.push(new Particle(mouse.x, mouse.y, 5, 'red',
+    particles.push(new Particle(mouse.x, mouse.y, 5, `hsl(${Math.random() * 360},50%,50%)`,
+      // particles.push(new Particle(mouse.x, mouse.y, 5, 'red',
       {
-        x: Math.cos(angleIncrement * i) * Math.random(),
-        y: Math.sin(angleIncrement * i) * Math.random()
+        // x: Math.cos(angleIncrement * i) * Math.random(),
+        x: Math.cos(angleIncrement * i) * Math.random() * power,
+        // y: Math.sin(angleIncrement * i) * Math.random()
+        y: Math.sin(angleIncrement * i) * Math.random() * power
       }))
 
   }
@@ -43,14 +46,19 @@ class Particle {
     this.velocity = velocity
     this.gravity = 0.005
     this.friction = 0.999
+    this.opacity = 1
   }
 
   draw() {
+    ctx.save()
+    ctx.globalAlpha = this.opacity
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     ctx.fillStyle = this.color
     ctx.fill()
     ctx.closePath()
+    ctx.restore()
+
   }
 
   update() {
@@ -60,18 +68,13 @@ class Particle {
     this.velocity.y += this.gravity
     this.x += this.velocity.x
     this.y += this.velocity.y
+    this.opacity -= 0.005
   }
 }
 
 // Implementation
-let particles
-function init() {
-  particles = []
+let particles = []
 
-  for (let i = 0; i < 400; i++) {
-    // particles.push()
-  }
-}
 
 // Animation Loop
 function animate() {
@@ -79,10 +82,13 @@ function animate() {
   ctx.fillStyle = 'rgba(0,0,0,0.08)'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-  particles.forEach(particle => {
-    particle.update()
+  particles.forEach((particle, index) => {
+    if (particle.opacity > 0) {
+      particle.update()
+    } else {
+      particles.splice(index, 1)
+    }
   })
 }
 
-init()
 animate()
